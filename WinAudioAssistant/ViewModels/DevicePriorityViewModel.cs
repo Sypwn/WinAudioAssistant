52b2e2e4153ba7daa2b9326866758811cb4184fb
Sypwn <9245957+Sypwn@users.noreply.github.com>
2023-11-18 22:24:11 -0500

    Initial commit

---
+++ WinAudioAssistant/ViewModels/DevicePriorityViewModel.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WinAudioAssistant.ViewModels
+{
+    public class DevicePriorityViewModel : INotifyPropertyChanged
+    {
+        private bool _separateCommsPriority;
+        
+        public event PropertyChangedEventHandler? PropertyChanged;
+
+        public bool SeparateCommsPriority
+        {
+            get => _separateCommsPriority;
+            set
+            {
+                _separateCommsPriority = value;
+                OnPropertyChanged("SeparateCommsPriority");
+            }
+        }
+
+        protected virtual void OnPropertyChanged(string propertyName)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+    }
+}
---
+++ WinAudioAssistant/Views/DevicePriorityView.xaml.cs
@@ -0,0 +1,35 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using WinAudioAssistant.ViewModels;
+
+namespace WinAudioAssistant
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class DevicePriorityView : Window
+    {
+        public DevicePriorityView()
+        {
+            InitializeComponent();
+        }
+
+        private void SeparateCommsPriorityCheckBox_Checked(object sender, RoutedEventArgs e)
+        {
+            Height += MainGrid.RowDefinitions[1].ActualHeight + OutputPriorityListBox.ActualHeight;
+        }
+
+        private void SeparateCommsPriorityCheckBox_Unchecked(object sender, RoutedEventArgs e)
+        {
+            Height -= MainGrid.RowDefinitions[1].ActualHeight + OutputPriorityListBox.ActualHeight;
+        }
+    }
+}
